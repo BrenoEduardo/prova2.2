9dfc5af2f9fb654bae5b865faeaa29c2a920d358
BrenoEduardo <brenoeducm@hotmail.com>
2020-05-29 19:44:57 -0300

    Leitura de dados

--- src/prova2.java
+++ src/prova2.java
@@ -1,9 +1,43 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class prova2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		int ht;
+		float vh;
+		char sn;
+		do {
+			System.out.print("Nome: ");
+			nome = sc.next();
+			System.out.print("Horas trabalhadas: ");
+			ht = sc.nextInt();
+			while (ht < 0) {
+				System.out.print("Horas trabalhadas: ");
+				ht = sc.nextInt();
+			}
+			System.out.print("Valor por hora: ");
+			vh = sc.nextFloat();
+			while (vh < 0) {
+				System.out.print("Valor por hora: ");
+				vh = sc.nextFloat();
+			}
+			System.out.print("Digitar outro (S/N)? ");
+			sn = sc.next().charAt(0);
+			
+			
+		} while(sn == 'S');
+		
+		
+		
+		
+sc.close();
 }
+}
+
+
+
